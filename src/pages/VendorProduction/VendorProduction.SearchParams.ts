da3b5348e278ebcba27064e2c81a91d54d3027d5
Jorge Diaz <93jads@gmail.com>
2024-12-11 12:07:47 -0300

    Refactor VendorProduction: Add designParams for Anais Del Valle Archila Gonzalez and integrate into component for enhanced functionality

--- src/pages/VendorProduction/VendorProduction.SearchParams.ts
+++ src/pages/VendorProduction/VendorProduction.SearchParams.ts
@@ -5,6 +5,8 @@ import { SearchParams } from "../../types";
 
 const checkedForSubcoField = getCustomField("CHECKED FOR SUBCO");
 const asbuiltBillingStatusField = getCustomField("ASBUILT BILLING STATUS");
+const designBillingStatusField = getCustomField("DESIGN BILLING STATUS");
+
 const { anaisDelValleArchilaGonzalez } = vendors;
 
 export const asbuiltParamsForAnaisDelValleArchilaGonzalez: SearchParams = {
@@ -25,6 +27,28 @@ export const asbuiltParamsForAnaisDelValleArchilaGonzalez: SearchParams = {
       field_id: checkedForSubcoField.id,
       operator: "NOT ALL",
       value: [checkedForSubcoField.type_config?.options?.[0].id],
+    },
+  ]),
+};
+
+export const designParamsForAnaisDelValleArchilaGonzalez: SearchParams = {
+  page: "0",
+  "assignees[]": anaisDelValleArchilaGonzalez.id.toString(),
+  "list_ids[]": CCI_HS_LIST_ID,
+  include_closed: "true",
+  custom_fields: JSON.stringify([
+    {
+      field_id: designBillingStatusField.id,
+      operator: "ANY",
+      value: [
+        designBillingStatusField.type_config?.options?.[0].id,
+        designBillingStatusField.type_config?.options?.[1].id,
+      ],
+    },
+    {
+      field_id: checkedForSubcoField.id,
+      operator: "NOT ALL",
+      value: [checkedForSubcoField.type_config?.options?.[1].id],
     },
   ]),
 };
--- src/pages/VendorProduction/VendorProduction.tsx
+++ src/pages/VendorProduction/VendorProduction.tsx
@@ -1,5 +1,8 @@
 import { useFilteredTasks } from "../../hooks/useFilteredTasks";
-import { asbuiltParamsForAnaisDelValleArchilaGonzalez } from "./VendorProduction.SearchParams";
+import {
+  asbuiltParamsForAnaisDelValleArchilaGonzalez,
+  designParamsForAnaisDelValleArchilaGonzalez,
+} from "./VendorProduction.SearchParams";
 
 function VendorProduction() {
   const {
@@ -8,23 +11,47 @@ function VendorProduction() {
     error: anaisAsbuiltError,
   } = useFilteredTasks(asbuiltParamsForAnaisDelValleArchilaGonzalez);
 
+  const {
+    filteredTasks: designForAnaisDelValleArchilaGonzalez,
+    loading: anaisDesignLoading,
+    error: anaisDesignError,
+  } = useFilteredTasks(designParamsForAnaisDelValleArchilaGonzalez);
+
   return (
-    <div>
-      {anaisAsbuiltError && <div>Error: {anaisAsbuiltError.message}</div>}
-      {anaisAsbuiltLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <div>
-          <h1>Asbuilts for Anais Del Valle Archila Gonzalez</h1>
-          {asbuiltForAnaisDelValleArchilaGonzalez.map((task) => (
-            <div style={{ lineHeight: "1.5" }} key={task.id}>
-              <h2>{task.name}</h2>
-              <p>{task.description}</p>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <>
+      <div>
+        {anaisAsbuiltError && <div>Error: {anaisAsbuiltError.message}</div>}
+        {anaisAsbuiltLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            <h1>Asbuilts for Anais Del Valle Archila Gonzalez</h1>
+            {asbuiltForAnaisDelValleArchilaGonzalez.map((task) => (
+              <div style={{ lineHeight: "1.5" }} key={task.id}>
+                <h2>{task.name}</h2>
+                <p>{task.description}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <div>
+        {anaisDesignError && <div>Error: {anaisDesignError.message}</div>}
+        {anaisDesignLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            <h1>Designs for Anais Del Valle Archila Gonzalez</h1>
+            {designForAnaisDelValleArchilaGonzalez.map((task) => (
+              <div style={{ lineHeight: "1.5" }} key={task.id}>
+                <h2>{task.name}</h2>
+                <p>{task.description}</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
