4217ed510b41cc0e5b4fc324ed79bb90a73ddf54
Jorge Diaz <93jads@gmail.com>
2024-12-17 14:39:21 -0300

    Add search parameter functions for fetching as-built and design data for a given vendor in VendorProduction.SearchParams

--- src/pages/VendorProduction/VendorProduction.SearchParams.ts
+++ src/pages/VendorProduction/VendorProduction.SearchParams.ts
@@ -54,3 +54,53 @@ export const designParamsForAnaisDelValleArchilaGonzalez: SearchParams = {
     },
   ]),
 };
+
+export function getAsbuiltSearchParamsForVendor(
+  vendorId: string
+): SearchParams {
+  return {
+    page: "0",
+    "assignees[]": vendorId,
+    "list_ids[]": CCI_HS_LIST_ID,
+    include_closed: "true",
+    custom_fields: JSON.stringify([
+      {
+        field_id: asbuiltBillingStatusField.id,
+        operator: "ANY",
+        value: [
+          asbuiltBillingStatusField.type_config?.options?.[0].id,
+          asbuiltBillingStatusField.type_config?.options?.[1].id,
+        ],
+      },
+      {
+        field_id: checkedForSubcoField.id,
+        operator: "NOT ALL",
+        value: [checkedForSubcoField.type_config?.options?.[0].id],
+      },
+    ]),
+  };
+}
+
+export function getDesignSearchParamsForVendor(vendorId: string): SearchParams {
+  return {
+    page: "0",
+    "assignees[]": vendorId,
+    "list_ids[]": CCI_HS_LIST_ID,
+    include_closed: "true",
+    custom_fields: JSON.stringify([
+      {
+        field_id: designBillingStatusField.id,
+        operator: "ANY",
+        value: [
+          designBillingStatusField.type_config?.options?.[0].id,
+          designBillingStatusField.type_config?.options?.[1].id,
+        ],
+      },
+      {
+        field_id: checkedForSubcoField.id,
+        operator: "NOT ALL",
+        value: [checkedForSubcoField.type_config?.options?.[1].id],
+      },
+    ]),
+  };
+}
