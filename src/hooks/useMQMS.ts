33ce6441d0da26a158cc8dee2d2019a484dc1f4a
Jorge Diaz <93jads@gmail.com>
2024-12-27 10:30:10 -0300

    Refactor useMQMS hook to include useEffect for user login handling and enhance state management for better user auth processes

--- src/hooks/useMQMS.ts
+++ src/hooks/useMQMS.ts
@@ -1,34 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MQMSUser } from "../types/MQMS";
 
 export function useMQMS() {
   const [MQMSUser, setMQMSUser] = useState<MQMSUser | null>(null);
 
-  const body = JSON.stringify({
-    username: "P3192616",
-    password: "tJKd%58LW$8D",
-  });
-
-  const url = "https://mqms.corp.chartercom.com/api/login";
-
-  async function login() {
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body,
+  useEffect(() => {
+    const body = JSON.stringify({
+      username: "P3192616",
+      password: "tJKd%58LW$8D",
     });
 
-    if (!response.ok) {
-      throw new Error("Failed to fetch MQMS user");
+    const url = "https://mqms.corp.chartercom.com/api/login";
+
+    async function login() {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body,
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch MQMS user");
+      }
+
+      const data = await response.json();
+      setMQMSUser(data);
     }
 
-    const data = await response.json();
-    setMQMSUser(data);
-  }
-
-  login();
+    login();
+  }, []);
 
   return { MQMSUser };
 }
