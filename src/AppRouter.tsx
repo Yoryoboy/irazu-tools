f974878a17d5c7758314aeed2f97eb2ef71563a0
Jorge Diaz <93jads@gmail.com>
2024-12-27 09:08:00 -0300

    Add MqmsVerification route to AppRouter and create basic component for improved navigation within the application

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import TaskSync from "./pages/TaskSync/TaskSync";
 import VendorProduction from "./pages/VendorProduction/VendorProduction";
+import MqmsVerification from "./pages/MqmsVerification/MqmsVerification";
 
 function AppRouter() {
   return (
@@ -8,6 +9,7 @@ function AppRouter() {
       <Route path="/" element={<TaskSync />} />
       <Route path="/task-sync" element={<TaskSync />} />
       <Route path="/production-contratistas" element={<VendorProduction />} />
+      <Route path="/mqms-verification" element={<MqmsVerification />} />
     </Routes>
   );
 }
---
+++ src/pages/MqmsVerification/MqmsVerification.tsx
@@ -0,0 +1,5 @@
+function MqmsVerification() {
+  return <div></div>;
+}
+
+export default MqmsVerification;
