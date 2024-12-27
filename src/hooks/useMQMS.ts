366eb3567e784f2b2316c4c6715a389081759ff6
Jorge Diaz <93jads@gmail.com>
2024-12-27 10:50:44 -0300

    Enhance useMQMS hook to accept listOfSentTasks and implement task fetching logic for improved user task management

--- src/hooks/useMQMS.ts
+++ src/hooks/useMQMS.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { MQMSUser } from "../types/MQMS";
 
-export function useMQMS() {
+export function useMQMS(listOfSentTasks: string[] = []) {
   const [MQMSUser, setMQMSUser] = useState<MQMSUser | null>(null);
 
   useEffect(() => {
@@ -31,6 +31,40 @@ export function useMQMS() {
 
     login();
   }, []);
+
+  useEffect(() => {
+    if (!MQMSUser || listOfSentTasks.length === 0) {
+      return;
+    }
+
+    const { accessToken } = MQMSUser;
+    const body = JSON.stringify({
+      archiveBucket: "live",
+      externalID: listOfSentTasks,
+    });
+    const url =
+      "https://mqms.corp.chartercom.com/api/work-requests/search?srcTimezone=America/Buenos_Aires";
+    const headers = {
+      "Content-Type": "application/json",
+      Authorizations: `Bearer ${accessToken}`,
+    };
+
+    async function fetchMQMSTasks() {
+      const response = await fetch(url, {
+        method: "POST",
+        headers,
+        body,
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch MQMS tasks");
+      }
+
+      const data = await response.json();
+      console.log(data);
+    }
+    fetchMQMSTasks();
+  }, [MQMSUser, listOfSentTasks]);
 
   return { MQMSUser };
 }
--- src/types/MQMS.ts
+++ src/types/MQMS.ts
@@ -1,8 +1,76 @@
 export interface MQMSUser {
-  accessToken?: string;
+  accessToken: string;
   passwordReset?: boolean;
-  userId?: string;
+  userId: string;
   defaultView?: null;
   userRole?: string;
   isCompanyRep?: null;
+}
+
+export interface MQMSFetchTasksResponse {
+  status?: string;
+  message?: string;
+  data?: Data;
+}
+
+export interface Data {
+  count?: number;
+  results?: Result[];
+  aggregation?: unknown[];
+}
+
+export interface Result {
+  masterProjectName?: string;
+  constructionSubType?: string;
+  uuid: string;
+  pointsRolledOver?: boolean;
+  job: string;
+  jobname: string;
+  externalID: string;
+  secondaryExternalID: string;
+  workRequestName?: string;
+  receivedDate?: Date;
+  slaDate?: Date;
+  etaDate?: Date;
+  assignedDate?: Date;
+  customerDays?: number;
+  designDays?: number;
+  ouRegion?: string;
+  ouRegionMarketUUID?: string;
+  lob?: string;
+  software?: string;
+  status: string;
+  currentAssignedUser?: string;
+  currentAssignedUsername?: string;
+  company?: string;
+  module?: string;
+  plantProjectJobType?: string;
+  lobUUID?: string;
+  softwareUUID?: string;
+  plantProjectJobTypeUUID?: string;
+  priority?: string;
+  origin?: string;
+  groupPoints?: number;
+  previousWorkRequestModuleCompany?: string;
+  city?: string;
+  icon?: string[];
+  legacyDivision?: string;
+  rejectReason?: string;
+  rejectDate?: null;
+  p_qc?: string;
+  prism_QC?: number;
+  escalationAssignee?: string;
+  previousCompanyUUID?: string;
+  previousWorkRequestCompany?: string;
+  previousWorkRequestUser?: string;
+  previousWorkRequestModuleUser?: string;
+  proximityGroupId?: null;
+  hubName?: string;
+  nodeName?: string;
+  rejectTime?: number;
+  currentAssignedUUID?: string;
+  currentAssignedModuleUUID?: string;
+  marketProfileUUID?: string;
+  coordinatorUUID?: string;
+  partialFlag?: boolean;
 }
