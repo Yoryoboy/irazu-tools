7a618f8ba7b019b27a28b8c56862aa0d42a6d026
Jorge Diaz <93jads@gmail.com>
2025-01-03 12:27:18 -0300

    Enhance useMQMS to manage loading state and handle errors during tasks fetching, improving user experience and error tracking

--- src/hooks/useMQMS.ts
+++ src/hooks/useMQMS.ts
@@ -8,10 +8,12 @@ export function useMQMSFetchTasks(
   listOfSentTasks: string[] = []
 ) {
   const [MQMSTasks, setMQMSTasks] = useState<Result[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     async function fetchTasks() {
       if (!accessToken || listOfSentTasks.length === 0) {
+        setIsLoading(false);
         return;
       }
 
@@ -23,31 +25,37 @@ export function useMQMSFetchTasks(
         Authorizations: accessToken,
       };
 
-      if (listOfSentTasks.length <= 30) {
-        const body = JSON.stringify({
-          ...partialBody,
-          externalID: listOfSentTasks.join(),
-        });
-        const data = await fetchMQMSTasks(headers, url, body);
-        setMQMSTasks(data.data.results);
-      } else {
-        const chunkedTasks = splitTaskArray(listOfSentTasks, 30);
-        const allResults = await Promise.all(
-          chunkedTasks.map(async (chunk) => {
-            const body = JSON.stringify({
-              ...partialBody,
-              externalID: chunk.join(),
-            });
-            const data = await fetchMQMSTasks(headers, url, body);
-            return data.data.results;
-          })
-        );
-        setMQMSTasks(allResults.flat());
+      try {
+        if (listOfSentTasks.length <= 30) {
+          const body = JSON.stringify({
+            ...partialBody,
+            externalID: listOfSentTasks.join(),
+          });
+          const data = await fetchMQMSTasks(headers, url, body);
+          setMQMSTasks(data.data.results);
+        } else {
+          const chunkedTasks = splitTaskArray(listOfSentTasks, 30);
+          const allResults = await Promise.all(
+            chunkedTasks.map(async (chunk) => {
+              const body = JSON.stringify({
+                ...partialBody,
+                externalID: chunk.join(),
+              });
+              const data = await fetchMQMSTasks(headers, url, body);
+              return data.data.results;
+            })
+          );
+          setMQMSTasks(allResults.flat());
+        }
+      } catch (error) {
+        console.error("Error fetching MQMS tasks:", error);
+      } finally {
+        setIsLoading(false);
       }
     }
 
     fetchTasks();
   }, [accessToken, listOfSentTasks]);
 
-  return { MQMSTasks };
+  return { MQMSTasks, isLoading };
 }
