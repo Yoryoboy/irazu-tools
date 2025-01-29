d8e0d33b1d0260b30ac4aaf9f4bdd200544eca05
Jorge Diaz <93jads@gmail.com>
2025-01-29 11:14:24 -0300

    Refactor: Split search parameters for Bau and HS in MqmsListConfigForCheckApproved and update fetching logic accordingly

--- src/pages/MqmsVerification/MqmsListConfigForCheckApproved.tsx
+++ src/pages/MqmsVerification/MqmsListConfigForCheckApproved.tsx
@@ -4,22 +4,34 @@ import { SearchParams } from "../../types/SearchParams";
 import { CLICKUP_LIST_IDS } from "../../utils/config";
 import MqmsCheckApprovedTasks from "./MqmsCheckApprovedTasks";
 
-const searchParams: SearchParams = {
+const bauSearchParams: SearchParams = {
   "statuses[]": ["sent"],
+};
+
+const hsSearchParams: SearchParams = {
+  "statuses[]": ["sent", "redesign sent"],
 };
 
 function MqmsListConfigForCheckApproved() {
   const { accessToken } = useMQMSAuth();
-  const { clickUpTasks } = useFetchClickUpTasks(
+  const { clickUpTasks: bauClickUpTasks } = useFetchClickUpTasks(
     CLICKUP_LIST_IDS.cciBau,
-    searchParams
+    bauSearchParams
+  );
+  const { clickUpTasks: hsClickUpTasks } = useFetchClickUpTasks(
+    CLICKUP_LIST_IDS.cciHs,
+    hsSearchParams
   );
 
   return (
     <div>
       <MqmsCheckApprovedTasks
         accessToken={accessToken}
-        clickUpTasks={clickUpTasks}
+        clickUpTasks={bauClickUpTasks}
+      />
+      <MqmsCheckApprovedTasks
+        accessToken={accessToken}
+        clickUpTasks={hsClickUpTasks}
       />
     </div>
   );
