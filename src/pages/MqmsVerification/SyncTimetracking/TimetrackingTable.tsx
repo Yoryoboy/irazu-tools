7fa88f14592e9d252442e3ca58e8934c1fe8ceeb
Jorge Diaz <93jads@gmail.com>
2025-02-20 09:57:19 -0300

    Cleanup MqmsTimetracking and TimetrackingTable by removing commented code and adding duration calculations for improved clarity and functionality

--- src/pages/MqmsVerification/SyncTimetracking/TimetrackingTable.tsx
+++ src/pages/MqmsVerification/SyncTimetracking/TimetrackingTable.tsx
@@ -1,5 +1,6 @@
 import { DataGrid, GridRowsProp, GridColDef } from "@mui/x-data-grid";
 import { newTimeEntryPayload } from "../../../types/Task";
+import test from "node:test";
 
 interface Props {
   payloads: newTimeEntryPayload[];
@@ -12,38 +13,43 @@ function getRowsforMUITable(payloads: newTimeEntryPayload[]) {
   const testRow = [];
 
   for (const [clickUpID, values] of Object.entries(groupedPayload)) {
-    const duration = values?.reduce((acc, value) => {
-      const time = value.stop ? value.stop - value.start : value.duration;
+    const designDuration = values?.reduce((acc, value) => {
+      const time = value.stop ? value.stop - value.start : 0;
       return acc + (time ?? 0);
     }, 0);
 
-    if (duration) {
-      testRow.push({
-        clickUpID,
-        duration: (duration / 60 / 60 / 1000).toFixed(2),
-      });
-    }
+    const qcDuration = values?.reduce((acc, value) => {
+      const time = value.duration ?? 0;
+      return acc + (time ?? 0);
+    }, 0);
+
+    testRow.push({
+      id: clickUpID,
+      clickUpID,
+      designDuration: designDuration
+        ? (designDuration / 60 / 60 / 1000).toFixed(2)
+        : "0",
+      qcDuration: qcDuration ? (qcDuration / 60 / 60 / 1000).toFixed(2) : "0",
+    });
   }
+
+  return testRow;
 }
 
 function TimetrackingTable({ payloads }: Props) {
   const rows = getRowsforMUITable(payloads);
+  console.log("rows :", rows);
   const columns: GridColDef[] = [
     { field: "clickUpID", headerName: "ClickUpID", width: 150 },
-    { field: "duration", headerName: "Duration", width: 150 },
-    { field: "tag", headerName: "Tag", width: 150 },
+    { field: "designDuration", headerName: " Design Duration", width: 150 },
+    { field: "qcDuration", headerName: " QC Duration", width: 150 },
   ];
 
-  //   const rows: GridRowsProp = [
-  //     { id: 1, col1: "Hello", col2: "World" },
-  //     { id: 2, col1: "DataGridPro", col2: "is Awesome" },
-  //     { id: 3, col1: "MUI", col2: "is Amazing" },
-  //   ];
   return (
     <DataGrid
-      rows={rows ?? []}
       columns={columns}
-      rowCount={1}
+      rows={rows}
+      rowCount={rows?.length ?? 0}
       paginationMode="server"
       initialState={{
         pagination: {
