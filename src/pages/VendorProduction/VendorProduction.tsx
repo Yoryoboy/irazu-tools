189930c57a789dced4b38638701a4b6556b5acf8
Jorge <93jads@gmail.com>
2025-03-11 11:20:30 -0300

    Uncomment VendorProductionTable component in VendorProduction for improved visibility of vendor data integration and rendering

--- src/pages/VendorProduction/VendorProduction.tsx
+++ src/pages/VendorProduction/VendorProduction.tsx
@@ -66,7 +66,7 @@ function VendorProduction() {
         bau={bauForAnaisDelValleArchilaGonzalez}
         vendor={anaisDelValleArchilaGonzalez}
       />
-      {/* <VendorProductionTable
+      <VendorProductionTable
         asbuilts={asbuiltForAnaisDelValleArchilaGonzalez}
         designs={designForAnaisDelValleArchilaGonzalez}
         redesigns={redesignForAnaisDelValleArchilaGonzalez}
@@ -83,7 +83,7 @@ function VendorProduction() {
         designs={designForNathaly}
         redesigns={redesignForNathaly}
         vendor={nathaly}
-      /> */}
+      />
     </main>
   );
 }
