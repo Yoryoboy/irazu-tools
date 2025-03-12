3a9d60c39bd0b06df7fd741f1983292712582573
Jorge <93jads@gmail.com>
2025-03-12 10:50:55 -0300

    Remove hardcoded MQMS credentials from config.ts, streamlining configuration for better security and maintainability.

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,5 @@
 export const CLICKUP_API_AKEY = import.meta.env.VITE_CLICKUP_API_AKEY;
 
-export const MQMS_USERNAME = import.meta.env.VITE_MQMS_USER;
-export const MQMS_PASSWORD = import.meta.env.VITE_MQMS_PASSWORD;
-
 export const TEAM_ID = "3051792";
 
 export const CLICKUP_LIST_IDS = {
