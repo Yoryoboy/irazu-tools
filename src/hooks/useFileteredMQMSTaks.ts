8b28778d34e9aefa2ad0e24a9936b18b9aef7ee5
Jorge <93jads@gmail.com>
2025-05-01 09:12:55 -0300

    Modified useFileteredMQMSTaks so the error are logged instead of thrown

--- src/hooks/useFileteredMQMSTaks.ts
+++ src/hooks/useFileteredMQMSTaks.ts
@@ -1,43 +1,41 @@
-import { useState } from "react";
-import { MQMSTasksWithClickUpID, TaskDatum } from "../types/MQMS";
-import { ExtractedTaskFieldValues } from "../types/Task";
+import { useState } from 'react';
+import { MQMSTasksWithClickUpID, TaskDatum } from '../types/MQMS';
+import { ExtractedTaskFieldValues } from '../types/Task';
 
 export function useFileteredMQMSTaks(
   MQMSTasks: TaskDatum[],
   ClickUpSentTasks: ExtractedTaskFieldValues[]
 ) {
-  const [filteredMQMSTasksWithClickUpID, setFilteredMQMSTasksWithClickUpID] =
-    useState<MQMSTasksWithClickUpID[]>(
-      MQMSTasks.map((task) => {
-        const sentTask = ClickUpSentTasks.find(
-          (currSentTask) => currSentTask["WORK REQUEST ID"] === task.uuid
-        );
-
-        if (!sentTask?.id) {
-          return { ...task, clickUpID: "" };
-        }
-        return { ...task, clickUpID: sentTask?.id?.toString() };
-      })
-    );
-
-  const [closedAndPreclosedTasks, setClosedAndPreclosedTasks] = useState<
-    TaskDatum[]
+  const [filteredMQMSTasksWithClickUpID, setFilteredMQMSTasksWithClickUpID] = useState<
+    MQMSTasksWithClickUpID[]
   >(
-    MQMSTasks.filter(
-      (task) => task.status.name === "CLOSED" || task.status.name === "PRECLOSE"
-    )
-  );
-
-  const closedAndPreclosedTasksWithClickUpID: MQMSTasksWithClickUpID[] =
-    closedAndPreclosedTasks.map((task) => {
+    MQMSTasks.map(task => {
       const sentTask = ClickUpSentTasks.find(
-        (currSentTask) =>
-          currSentTask.name === task.externalID &&
-          currSentTask["SECONDARY ID"] === task.secondaryExternalID
+        currSentTask => currSentTask['WORK REQUEST ID'] === task.uuid
       );
 
       if (!sentTask?.id) {
-        throw new Error(
+        return { ...task, clickUpID: '' };
+      }
+      return { ...task, clickUpID: sentTask?.id?.toString() };
+    })
+  );
+
+  const [closedAndPreclosedTasks, setClosedAndPreclosedTasks] = useState<TaskDatum[]>(
+    MQMSTasks.filter(task => task.status.name === 'CLOSED' || task.status.name === 'PRECLOSE')
+  );
+
+  const closedAndPreclosedTasksWithClickUpID: MQMSTasksWithClickUpID[] =
+    closedAndPreclosedTasks.map(task => {
+      const sentTask = ClickUpSentTasks.find(
+        currSentTask =>
+          currSentTask.name === task.externalID &&
+          currSentTask['SECONDARY ID'] === task.secondaryExternalID
+      );
+
+      if (!sentTask?.id) {
+        console.log(sentTask);
+        console.log(
           `MQMS Task ${task.externalID} with secondary ID ${task.secondaryExternalID} not found in ClickUp sent tasks`
         );
       }
