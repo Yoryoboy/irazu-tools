9b14f0c07934832469615680fa439362b9aa8f5e
Jorge <93jads@gmail.com>
2025-07-02 10:32:55 -0300

    Update clickUpCustomFields and VendorProduction search parameters

--- src/pages/VendorProduction/VendorProduction.SearchParams.ts
+++ src/pages/VendorProduction/VendorProduction.SearchParams.ts
@@ -1,31 +1,30 @@
-import { getCustomField } from "../../utils/tasksFunctions";
-import { CLICKUP_LIST_IDS } from "../../utils/config";
-import { SearchParams } from "../../types/SearchParams";
+import { getCustomField } from '../../utils/tasksFunctions';
+import { CLICKUP_LIST_IDS } from '../../utils/config';
+import { SearchParams } from '../../types/SearchParams';
 
-const asbuiltChecked = getCustomField("ASBUILT CHECKED");
-const designChecked = getCustomField("DESIGN CHECKED");
-const redesignChecked = getCustomField("REDESIGN CHECKED");
-const bauChecked = getCustomField("BAU CHECKED");
-const asbuiltBillingStatusField = getCustomField("ASBUILT BILLING STATUS");
-const designBillingStatusField = getCustomField("DESIGN BILLING STATUS");
-const redesignBillingStatusField = getCustomField("REDESIGN BILLING STATUS");
-const designAssigneeField = getCustomField("DESIGN ASSIGNEE");
-const bauBillingStatusField = getCustomField("BAU BILLING STATUS");
+const asbuiltChecked = getCustomField('ASBUILT CHECKED');
+const designChecked = getCustomField('DESIGN CHECKED');
+const redesignChecked = getCustomField('REDESIGN CHECKED');
+const bauChecked = getCustomField('BAU CHECKED');
+const asbuiltBillingStatusField = getCustomField('ASBUILT BILLING STATUS');
+const designBillingStatusField = getCustomField('DESIGN BILLING STATUS');
+const redesignBillingStatusField = getCustomField('REDESIGN BILLING STATUS');
+const designAssigneeField = getCustomField('DESIGN ASSIGNEE');
+const bauBillingStatusField = getCustomField('BAU BILLING STATUS');
+const customerCompanyField = getCustomField('Customer Company');
 
 // Anais Del Valle Archila Gonzalez
 
-export function getAsbuiltSearchParamsForVendor(
-  vendorId: string
-): SearchParams {
+export function getAsbuiltSearchParamsForVendor(vendorId: string): SearchParams {
   return {
-    page: "0",
-    "assignees[]": vendorId,
-    "list_ids[]": CLICKUP_LIST_IDS.cciHs,
-    include_closed: "true",
+    page: '0',
+    'assignees[]': vendorId,
+    'list_ids[]': CLICKUP_LIST_IDS.cciHs,
+    include_closed: 'true',
     custom_fields: JSON.stringify([
       {
         field_id: asbuiltBillingStatusField.id,
-        operator: "ANY",
+        operator: 'ANY',
         value: [
           asbuiltBillingStatusField.type_config?.options?.[0].id,
           asbuiltBillingStatusField.type_config?.options?.[1].id,
@@ -33,7 +32,7 @@ export function getAsbuiltSearchParamsForVendor(
       },
       {
         field_id: asbuiltChecked.id,
-        operator: "IS NULL",
+        operator: 'IS NULL',
       },
     ]),
   };
@@ -41,18 +40,18 @@ export function getAsbuiltSearchParamsForVendor(
 
 export function getDesignSearchParamsForVendor(vendorId: string): SearchParams {
   return {
-    page: "0",
-    "list_ids[]": CLICKUP_LIST_IDS.cciHs,
-    include_closed: "true",
+    page: '0',
+    'list_ids[]': CLICKUP_LIST_IDS.cciHs,
+    include_closed: 'true',
     custom_fields: JSON.stringify([
       {
         field_id: designAssigneeField.id,
-        operator: "ANY",
+        operator: 'ANY',
         value: [vendorId],
       },
       {
         field_id: designBillingStatusField.id,
-        operator: "ANY",
+        operator: 'ANY',
         value: [
           designBillingStatusField.type_config?.options?.[0].id,
           designBillingStatusField.type_config?.options?.[1].id,
@@ -60,24 +59,22 @@ export function getDesignSearchParamsForVendor(vendorId: string): SearchParams {
       },
       {
         field_id: designChecked.id,
-        operator: "IS NULL",
+        operator: 'IS NULL',
       },
     ]),
   };
 }
 
-export function getRedesignSearchParamsForVendor(
-  vendorId: string
-): SearchParams {
+export function getRedesignSearchParamsForVendor(vendorId: string): SearchParams {
   return {
-    page: "0",
-    "assignees[]": vendorId,
-    "list_ids[]": CLICKUP_LIST_IDS.cciHs,
-    include_closed: "true",
+    page: '0',
+    'assignees[]': vendorId,
+    'list_ids[]': CLICKUP_LIST_IDS.cciHs,
+    include_closed: 'true',
     custom_fields: JSON.stringify([
       {
         field_id: redesignBillingStatusField.id,
-        operator: "ANY",
+        operator: 'ANY',
         value: [
           redesignBillingStatusField.type_config?.options?.[0].id,
           redesignBillingStatusField.type_config?.options?.[1].id,
@@ -85,7 +82,7 @@ export function getRedesignSearchParamsForVendor(
       },
       {
         field_id: redesignChecked.id,
-        operator: "IS NULL",
+        operator: 'IS NULL',
       },
     ]),
   };
@@ -93,14 +90,14 @@ export function getRedesignSearchParamsForVendor(
 
 export function getBAUSearchParamsForVendor(vendorId: string): SearchParams {
   return {
-    page: "0",
-    "assignees[]": vendorId,
-    "list_ids[]": CLICKUP_LIST_IDS.cciBau,
-    include_closed: "true",
+    page: '0',
+    'assignees[]': vendorId,
+    'list_ids[]': CLICKUP_LIST_IDS.cciBau,
+    include_closed: 'true',
     custom_fields: JSON.stringify([
       {
         field_id: bauBillingStatusField.id,
-        operator: "ANY",
+        operator: 'ANY',
         value: [
           bauBillingStatusField.type_config?.options?.[0].id,
           bauBillingStatusField.type_config?.options?.[1].id,
@@ -108,7 +105,12 @@ export function getBAUSearchParamsForVendor(vendorId: string): SearchParams {
       },
       {
         field_id: bauChecked.id,
-        operator: "IS NULL",
+        operator: 'IS NULL',
+      },
+      {
+        field_id: customerCompanyField.id,
+        operator: '=',
+        value: customerCompanyField.type_config?.options?.find(option => option.name === 'CCI')?.id,
       },
     ]),
   };
